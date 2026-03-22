fd00b0bf9d2fe21c30f7bde92b30faf2b06794c9
Hemant Agarwal <hemantbansal1992@gmail.com>
2026-03-22 19:58:45 +0530

    Added end to end ai agents using langchain

--- Langchain_folder/6_langchain_output_parsers/structuredoutputparser.py
+++ Langchain_folder/6_langchain_output_parsers/structuredoutputparser.py
@@ -1,8 +1,7 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import PydanticOutputParser
-from pydantic import BaseModel, Field
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 
 load_dotenv()
 
@@ -14,22 +13,22 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm=llm)
 
-class Person(BaseModel):
+schema = [
+    ResponseSchema(name='fact_1', description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2', description='Fact 2 about the topic'),
+    ResponseSchema(name='fact_3', description='Fact 3 about the topic'),
+]
 
-    name: str = Field(description='Name of the person')
-    age: int = Field(gt=18, description='Age of the person')
-    city: str = Field(description='Name of the city the person belongs to')
-
-parser = PydanticOutputParser(pydantic_object=Person)
+parser = StructuredOutputParser.from_response_schemas(schema)
 
 template = PromptTemplate(
-    template='Generate the name, age and city of a fictional {place} person \n {format_instruction}',
-    input_variables=['place'],
+    template='Give 3 fact about {topic} \n {format_instruction}',
+    input_variables=['topic'],
     partial_variables={'format_instruction':parser.get_format_instructions()}
 )
 
 chain = template | model | parser
 
-final_result = chain.invoke({'place':'sri lankan'})
+result = chain.invoke({'topic':'black hole'})
 
-print(final_result)
+print(result)
